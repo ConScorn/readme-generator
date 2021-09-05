b478cf588d5b1eafb7676f6f912c3969cb68f2af
ConScorn <connorscorn@hotmail.com>
2021-09-05 04:01:07 -0500

    finished project, pulls answers from prompt

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# readme-generator
-An application designed to quickly generate a ReadME based on user input.
+# README generator
+     
+## Description
+
+This app generates a README file, based on user input. It was designed to allow developers to quickly create effective README files without devoting large amounts of time.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributions](#contributions)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+Here is the link to the Github repository: (undefined)
+Here is the link to the deployed application: (undefined)
+
+## Usage
+
+open it
+
+## Tests
+
+undefined
+
+## Contributions
+
+
+
+## License
+
+MIT
+
+## Questions
+
+Here is a link to my GitHub profile: https://github.com/ConScorn. Otherwise, you can email me at: test@test.com
---
+++ index.js
@@ -0,0 +1,112 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+const generateReadME = (answers) =>
+`# ${answers.title}
+     
+## Description
+
+${answers.desc}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributions](#contributions)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+Here is the link to the Github repository: (${answers.repolink})
+Here is the link to the deployed application: (${answers.deploylink})
+
+## Usage
+
+${answers.usage}
+
+## Tests
+
+${answers.test}
+
+## Contributions
+
+${answers.contributors}
+
+## License
+
+${answers.license}
+
+## Questions
+
+Here is a link to my GitHub profile: https://github.com/${answers.github}. Otherwise, you can email me at: ${answers.email}
+`;
+
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'How would you describe you project? (What does it do, what pushed you to create it, and how does it work?)'
+        },
+        {
+            type: 'input',
+            name: 'repoLink',
+            message: 'Enter the link to your gitHub repository here:'
+        },
+        {
+            type: 'input',
+            name: 'deployLink',
+            message: 'Enter the link to your deployed application here:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How would someone use your project?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'If you have any tests for your app, enter them here:'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Would you like others to contribute on this project? Enter your stance here:'
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Please select a license for your project:',
+            choices: ["MIT", "ISC", "Apache license 2.0", "zLib",]
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username here:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email here:'
+        },
+    ])
+    .then((answers) => {
+        const readMePage = generateReadME(answers);
+
+        fs.writeFile("README.md", readMePage, (err) =>
+            err ? console.log(err) : console.log("README generated successfully!"));
+    });
+
+
+    // function writeToFile(fileName, data) {}
